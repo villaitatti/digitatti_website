c29e2f13d141f946ce82ea16b9f6c21c09192b32
Remo Grillo <remogrillo@MacBook-Pro-di-Remo-16.local>
2020-10-15 17:58:03 +0200

    Added fellows

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ var projects = [
         "picture":"Archipelago.png"
     },
     {
-        "title":"Florentia Illustrata",
+        "title":"Florentia illustrata: spatializing history & visualizing experience in the renaissance city",
         "description":"Florentia Illustrata was established in 2018 by common agreement among its founding members in recognition of the necessity to work together to create a research platform to provide consolidated access to digital projects on Renaissance Florence. Using the rich archival resources of the city, each of these approaches to reconstructing the past have been exploring the potential of digital tools to expand and innovate the methods used in humanistic historical research. Building on the long tradition of cutting-edge research techniques that have been brought to bear on Florence’s singular documentary and material legacy, they will create a shared digital platform that enables an intensive analysis of complex interrelated historical phenomena that characterized the early modern city. As such, they will lay the groundwork for innovative studies that will be able to embed experiments in urban planning, architectural design, and artistic representation within the concrete social structures, historical experiences, and spatial mentalities of the city’s inhabitants.",
         "picture":"florentia_illustrata.jpg"
     },
@@ -15,7 +15,7 @@ var projects = [
         "picture":"florentine_drawings.001.png"
     },
     {
-        "title":"Pharos",
+        "title":"PHAROS: an international consortium committed to creating an open and freely accessible digital research platform",
         "description":"PHAROS is an international consortium of fourteen European and North American art historical photo archives committed to creating an open and freely accessible digital research platform allowing for comprehensive consolidated access to photo archive images and their associated scholarly documentation.  The PHAROS collections collectively contain an estimated 25 million images documenting works of art and architecture and the history of photography itself. The consortium is actively looking for funding and technology partners to advance this work.",
         "picture":"pharos.jpg"
     },
@@ -23,6 +23,11 @@ var projects = [
         "title":"Semantic Digital Publishing",
         "description":"The Semantic Digital Publishing project links textual resources to data contained in both internal and external knowledge graphs, under the form of an annotation. If a chunk of text describes a person, place, event, or other kinds of entities, it can be manually linked to the URI of that entity, and vice versa. Other than pure textual annotations, the Semantic Digital Publishing allows users to also create complex annotations. Where the goal is the linkage between free text, RDF data, and images. In particular, this project links parts of two distinct documents, or to a region of an image that is displayed through an IIIF Viewer. ",
         "picture":"semantic-publishing.001.png"
+    },
+    {
+        "title":"CRIA (committee to rescue italian art): an online exhibition that explores the aftermath of a devastating flood in Florence, Italy in 1966",
+        "description":"Following the devastating flood of '66, Villa I Tatti became the Florentine Headquarters of CRIA (Committee to Rescue Italian Art), a task force made up of scholars anxious to preserve Florence's precious artistic heritage. Amidst the turmoil and with a deep sense of urgency, they organized both a vast fund-raising campaign and the shipment to Italy of emergency conservation materials along with a team of specialists to assess the situation and begin long-range planning for the recovery. Chaired by Jacqueline Kennedy, the Committee raised money through direct mail requests, ads, special exhibits of Italian art, fashion shows and cocktail parties, in addition to video appeals by famous figures such as Ted Kennedy and Elizabeth Taylor. This online exhibition explores the valiant efforts of CRIA during the aftermath of the flood, and examines I Tatti's central role in the recovery.",
+        "picture":"cria.jpg"
     },
     {
         "title":"Semantic Vision: Linking Artworks for the Web of Data",
@@ -44,10 +49,31 @@ var projects = [
         "description":"",
         "picture":""
     },
-    {
-        "title":"",
-        "description":"",
-        "picture":""
-    },
 ];
-var people = {}
+
+var fellows = [
+    {
+        "name":"Lukas Klic",
+        "title":"Manager of Information Services & Digital Initiatives \n Digital Research Infrastructure for the Humanities",
+        "bio":"Lukas Klic is Manager of Information Services & Digital Initiatives at I Tatti. His research focuses on digital tools that facilitate the accessibility and interpretation of cultural heritage, leveraging Linked Open Data (LOD) to allow for the cross-pollination of research data. He was the co-principal investigator of the The Drawings of the Florentine Painters, a project that transformed and published Bernard Berenson’s work in a semantically enriched digital edition. He holds an MLIS from Simmons University, and a PhD in Computer Science from Ca’ Foscari University with a concentration in the Digital Humanities. At I Tatti he works on a number of research projects involving linked open data, computer vision, natural language processing, geospatial mapping, and digital publishing.",
+        "picture":"LukasKlic.JPG"
+    },
+    {
+        "name":"Ludovica Galeazzo",
+        "title":"Digital Humanities Research Associate (2019-2021) \n Archipelago: Mapping the Islands of the Venetian Lagoon",
+        "bio":"Ludovica Galeazzo is an architectural and urban historian whose research focuses on Venetian architecture in the early modern period with a special interest in new technologies to demonstrate the process of city’s change over time. She was a Kress Digital Humanities Fellow at I Tatti in 2019, and currently holds an appointment as a Digital Humanities Research Associate. She received her PhD in Architectural History from the Graduate School Ca’ Foscari-Iuav in Venice and was later a Research Fellow at the Iuav University of Architecture in Venice (2014-2016) and a Postdoctoral Associate at Duke University (2016-2017). She worked as assistant curator on three international exhibitions on early modern Venetian history displayed at the Ducal Palace in Venice and the Nasher Museum of Art at Duke. Since 2011, she has been a member of the international research project Visualizing Venice/Visualizing Cities. She is author of Venezia e I margini urbani. L’insula dei Gesuiti in età moderna (2018) and co-editor of Acqua e cibo a Venezia. Storie della laguna e della città (2015).",
+        "picture":"LudovicaGaleazzo.jpg"
+    },
+    {
+        "name":"Gianmarco Spinaci",
+        "title":"Digital Humanities Research Associate (2019-2021) \n Digital Research Infrastructure for the Humanities",
+        "bio":"Gianmarco Spinaci is currently pursuing a Master’s degree in Digital Humanities and Digital Knowledge at the University of Bologna. He holds a Bachelor’s degree in Computer Science and Management. Since graduating, he has been a consultant in the Digital and Semantic Publishing Laboratory (DASPlab), working on the project called RAJE: RASH Javascript Editor. As an intern at the Centre for Science and Technology Studies (CWTS) in Leiden, The Netherlands, he worked on identifying Arts and Humanities publications within the main publication databases; he is further developing this work for his Master’s degree thesis.",
+        "picture":"GianmarcoSpinaci.jpg"
+    },
+    {
+        "name":"Remo Grillo",
+        "title":"Digital Humanities Research Associate (2019-2021) \n Digital Research Infrastructure for the Humanities",
+        "bio":"Remo Grillo holds a Master’s degree in Digital Humanities and Digital Knowledge with honors from the Alma Mater Studiorum - University of Bologna. His research focuses on knowledge representation and extraction through ontologies, as well as on natural language processing and algorithms applied to music, museum artifacts, and literature. He worked as a Full-stack Developer while attending the University of Naples Federico II and Ludwig Maximillian University of Munich, where he obtained a Bachelor’s degree with a thesis on Logic, applied to a digital critical edition of the Tractatus Logico-Philosophicus by Ludwig Wittgenstein.",
+        "picture":"RemoGrillo.jpg"
+    }
+];
--- src/main.js
+++ src/main.js
@@ -11,8 +11,14 @@ $(document).ready(function(){
             });
         }
     });
-
     showProjects();
+    showFellows();
+
+    $('#mainCol1, #mainCol2').fadeIn(2000).animate({
+        'margin-bottom': '5%'
+    }, {duration: 1500, queue: false}, function() {
+        // Animation complete.
+    });
 });
 
 
@@ -21,8 +27,22 @@ function showProjects(){
         let projecthtml = "<div class='projectContainer'>";
         projecthtml += "<img class='projectImage' src='assets/images/projects/" + v.picture +"'>";
         projecthtml += "<p class='projectTitle'>" + v.title + "</p>";
+        projecthtml += "<p class='projectDescription' style='display:none;'>" + v.description + "</p>";
         projecthtml += "</div>";
 
         $('#projectsContainer').append(projecthtml);
+    });
+}
+
+function showFellows(){
+    $.each(fellows, function(k,v){
+        let fellowhtml = "<div class='fellowContainer'>";
+        fellowhtml += "<img class='fellowImage' src='assets/images/fellows/" + v.picture +"'>";
+        fellowhtml += "<p class='fellowName'>" + v.name + "</p>";
+        fellowhtml += "<p class='fellowTitle' style='display:none;'>" + v.title + "</p>";
+        fellowhtml += "<p class='fellowBio' style='display:none;'>" + v.description + "</p>";
+        fellowhtml += "</div>";
+
+        $('#fellowsContainer').append(fellowhtml);
     });
 }
