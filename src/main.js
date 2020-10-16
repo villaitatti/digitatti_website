cffb83c895aa59844f024906a6b7e0c08d6203fa
Remo Grillo <remogrillo@MacBook-Pro-di-Remo-16.local>
2020-10-16 21:57:13 +0200

    added arc animations and libraries

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 1500, function(){
+            }, 1000, function(){
                 window.location.hash = hash;
             });
         }
@@ -14,44 +14,67 @@ $(document).ready(function(){
     showProjects();
     showFellows();
 
-    setTimeout(function(){$('#subtitle').animate({
-        'margin-bottom': '15px', 'opacity': '1'
-    }, {duration: 1500, queue: false}, function() {
-        // Animation complete.
-    });}, 1500);
 
 // Wrap every letter in a span
     var textWrapper = document.querySelector('.ml11 .letters');
     textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
-    anime.timeline({loop: false})
-        .add({
-            targets: '.ml11 .line',
-            scaleY: [0,1],
-            opacity: [0.5,1],
+
+    setTimeout(function(){
+        anime.timeline({loop: false})
+            .add({
+                targets: '.ml11 .line',
+                scaleY: [0,1],
+                opacity: [0.5,1],
+                easing: "easeOutExpo",
+                duration: 700
+            })
+            .add({
+                targets: '.ml11 .line',
+                translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+                easing: "easeOutExpo",
+                duration: 700,
+                delay: 100
+            }).add({
+            targets: '.ml11 .letter',
+            opacity: [0,1],
             easing: "easeOutExpo",
-            duration: 700
-        })
-        .add({
-            targets: '.ml11 .line',
-            translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-            easing: "easeOutExpo",
-            duration: 700,
-            delay: 100
+            duration: 600,
+            offset: '-=775',
+            delay: (el, i) => 34 * (i+1)
         }).add({
-        targets: '.ml11 .letter',
-        opacity: [0,1],
-        easing: "easeOutExpo",
-        duration: 600,
-        offset: '-=775',
-        delay: (el, i) => 34 * (i+1)
-    }).add({
-        targets: '.line',
-        opacity: 0,
-        duration: 600,
-        easing: "easeOutExpo",
-        delay: 600
+            targets: '.line',
+            opacity: 0,
+            duration: 600,
+            easing: "easeOutExpo",
+            delay: 600
+        });
+
+        setTimeout(function(){$('#subtitle').animate({
+            'margin-bottom': '15px', 'opacity': '1'
+        }, {duration: 1500, queue: false}, function() {
+            // Animation complete.
+        });}, 1000);
+
+
+    }, 1000);
+
+
+    $('.projectContainer').click(function(){
+        fillProjectModal(this);
     });
+
+    var arcanimation = document.getElementById('arcanimation');
+    // Set up our animation
+
+    var animData = {
+        container: arcanimation,
+        renderer: 'svg',
+        autoplay: true,
+        loop: false,
+        path : 'assets/images/logoanimated1.json'
+    };
+    var anim = bodymovin.loadAnimation(animData);
 });
 
 
@@ -78,4 +101,18 @@ function showFellows(){
 
         $('#fellowsContainer').append(fellowhtml);
     });
+}
+
+
+function fillProjectModal(project){
+    let projectModal = $("#projectModal");
+    let image = $(project).find('.projectImage').attr("src");
+    let title = $(project).find('.projectTitle').text();
+    let desc = $(project).find('.projectDescription').text();
+
+    $('#projectModalImage').attr("src", image);
+    $('#projectModalTitle').text(title);
+    $('#projectModalDescription').text(desc);
+
+    projectModal.modal('show');
 }
