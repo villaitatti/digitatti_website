026f1bd26832816eccb8cb86cbb69e555273d399
gspinaci <gianmarco.spinaci@gmail.com>
2021-01-15 17:42:47 +0100

    Update ordine

--- src/data.js
+++ src/data.js
@@ -2,13 +2,13 @@ var projects = [
     {
         "title":"Metapolis: semantic research infrastructure",
         "description":"In the early modern period, the network of islands encircling the Venetian lagoon served as capillary structures for the political, socio-economic, and cultural interests of the Serenissima. Scattered throughout the entire ‘gulf’ of Venice, these settlements were indispensable to the larger Venetian community as loci dedicated to the city’s food supply, spiritual places for religious communities, and centers for defense structures or public hospitals. The socio-political events that followed the fall of the Republic (1797) profoundly changed this understanding and totally altered the reading of the city as an organic entity that encompasses the watery ecosystem. In some cases, interventions significantly transformed the islands’ geographic configuration and functions. The Archipelago project aims to investigate the long-term history and transformation of the clusters of settlements scattered throughout Venice’s lagoon between the late fifteenth and seventeenth centuries through a systematic and geographic-based analysis. This combines historical and digital methods by employing a semantic research platform for mapping, visualizing, and exploring urban data over time.",
-        "picture":"metapolis.jpg",
-        "size": "big"
+        "picture":"metapolis.jpg"
     },
     {
-        "title":"Archipelago: Mapping the Islands of the Venetian Lagoon",
-        "description":"In the early modern period, the network of islands encircling the Venetian lagoon served as capillary structures for the political, socio-economic, and cultural interests of the Serenissima. Scattered throughout the entire ‘gulf’ of Venice, these settlements were indispensable to the larger Venetian community as loci dedicated to the city’s food supply, spiritual places for religious communities, and centers for defense structures or public hospitals. The socio-political events that followed the fall of the Republic (1797) profoundly changed this understanding and totally altered the reading of the city as an organic entity that encompasses the watery ecosystem. In some cases, interventions significantly transformed the islands’ geographic configuration and functions. The Archipelago project aims to investigate the long-term history and transformation of the clusters of settlements scattered throughout Venice’s lagoon between the late fifteenth and seventeenth centuries through a systematic and geographic-based analysis. This combines historical and digital methods by employing a semantic research platform for mapping, visualizing, and exploring urban data over time.",
-        "picture":"Archipelago.png"
+        "title":"Florentia illustrata: spatializing history & visualizing experience in the renaissance city",
+        "description":"Florentia Illustrata was established in 2018 by common agreement among its founding members in recognition of the necessity to work together to create a research platform to provide consolidated access to digital projects on Renaissance Florence. Using the rich archival resources of the city, each of these approaches to reconstructing the past have been exploring the potential of digital tools to expand and innovate the methods used in humanistic historical research. Building on the long tradition of cutting-edge research techniques that have been brought to bear on Florence’s singular documentary and material legacy, they will create a shared digital platform that enables an intensive analysis of complex interrelated historical phenomena that characterized the early modern city. As such, they will lay the groundwork for innovative studies that will be able to embed experiments in urban planning, architectural design, and artistic representation within the concrete social structures, historical experiences, and spatial mentalities of the city’s inhabitants.",
+        "picture":"florentia_illustrata.jpg",
+        "size": "big"
     },
     {
         "title":"Gesture in Italian Early Renaissance Art: a Digital Perspective",
@@ -18,10 +18,13 @@ var projects = [
             "Digital Humanities Fellow (2018-2019)"
     },
     {
-        "title":"Florentia illustrata: spatializing history & visualizing experience in the renaissance city",
-        "description":"Florentia Illustrata was established in 2018 by common agreement among its founding members in recognition of the necessity to work together to create a research platform to provide consolidated access to digital projects on Renaissance Florence. Using the rich archival resources of the city, each of these approaches to reconstructing the past have been exploring the potential of digital tools to expand and innovate the methods used in humanistic historical research. Building on the long tradition of cutting-edge research techniques that have been brought to bear on Florence’s singular documentary and material legacy, they will create a shared digital platform that enables an intensive analysis of complex interrelated historical phenomena that characterized the early modern city. As such, they will lay the groundwork for innovative studies that will be able to embed experiments in urban planning, architectural design, and artistic representation within the concrete social structures, historical experiences, and spatial mentalities of the city’s inhabitants.",
-        "picture":"florentia_illustrata.jpg"
+        "title":"Archipelago: Mapping the Islands of the Venetian Lagoon",
+        "description":"In the early modern period, the network of islands encircling the Venetian lagoon served as capillary structures for the political, socio-economic, and cultural interests of the Serenissima. Scattered throughout the entire ‘gulf’ of Venice, these settlements were indispensable to the larger Venetian community as loci dedicated to the city’s food supply, spiritual places for religious communities, and centers for defense structures or public hospitals. The socio-political events that followed the fall of the Republic (1797) profoundly changed this understanding and totally altered the reading of the city as an organic entity that encompasses the watery ecosystem. In some cases, interventions significantly transformed the islands’ geographic configuration and functions. The Archipelago project aims to investigate the long-term history and transformation of the clusters of settlements scattered throughout Venice’s lagoon between the late fifteenth and seventeenth centuries through a systematic and geographic-based analysis. This combines historical and digital methods by employing a semantic research platform for mapping, visualizing, and exploring urban data over time.",
+        "picture":"Archipelago.png",
+        "size": "big"
     },
+
+
     {
         "title":"The Drawings of the Florentine Painters",
         "description":"Bernard Berenson’s Drawings of the Florentine Painters is a digital resource based on Berenson’s publication of the same name. This research tool allows you to search for any of the drawings listed in the three editions (1903, 1938 in English; 1961, in Italian). Filters allow you to search Berenson’s information by artist, title (in English or Italian), location, and technique. Each entry includes key information from all three editions of Berenson’s text, as well as the current location, an image of the catalog page, and plates included in 1903; most entries also have links to museum webpages, including images. For information about how the data was prepared, how to use the site, and for background on Berenson’s scholarship, see the About page.",
@@ -34,7 +37,6 @@ var projects = [
         "description":"I Tatti is a key partner within PHAROS, an international consortium of fourteen European and North American art historical photo archives committed to creating an open and freely accessible digital research platform allowing for comprehensive consolidated access to photo archive images and their associated scholarly documentation.  The PHAROS collections collectively contain an estimated 25 million images documenting works of art and architecture and the history of photography itself. The consortium is actively looking for funding and technology partners to advance this work.",
         "picture":"pharos.jpg",
         "website":"http://pharosartresearch.org/",
-        "size": "big"
     },
     {
         "title":"Semantic Digital Publishing",
